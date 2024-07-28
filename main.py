8bff70bdc8541f0aeb4a4f6f8f7ee2cab50910d6
Mikael-UofA <lnineza@ualberta.ca>
2024-07-28 17:30:18 -0600

    prototype of main.py complete

--- main.py
+++ main.py
@@ -1,35 +1,55 @@
 from course import Course
-from interactions import main_menu, year_selection, sem_selection, course_selection, create_course, course_lookup;
-def main():
-    choice = main_menu()
+import interactions
+import database
 
+def main():
+    if (database.initialize_database()):
+        print("Terminating operations...")
+        return -1
+    
+
+    choice = interactions.main_menu()
+
+    # Main Menu
     while choice != 0:
+        # View courses
         if choice == 1:
             # find the years in the db
-            choice = year_selection(["I", "II", "III", "IV"])
+            years = database.get_years()
+            choice = interactions.year_selection(years)
 
             if choice != 0:
-                sem = sem_selection()
+                year = years[choice - 1]
+                sem = interactions.sem_selection()
+                
+                courses = database.get_courses(year, sem)
                 # find the courses in this year and semester
-                choice = course_selection(["Course1", "Course2", "Course3"])
-
+                choice = interactions.course_selection(courses)
                 if choice != 0:
                     # show the course
-                    print(f"Showing details for {['Course1', 'Course2', 'Course3'][choice-1]}")
+                    my_course = courses[choice - 1]
+                    my_course = Course(*my_course)
+                    my_course.print()
+
+        # Add a course
         elif choice == 2:
-            new_course = create_course()
+            new_course = interactions.create_course()
             # store this new course
             print("Storing new course:")
             new_course.print()
+
+        # Course lookup
         elif choice == 3:
-            course = course_lookup()
+            course = interactions.course_lookup()
             # return this course if it exists
             print(f"Lookup result for {course[0]} {course[1]}")
+
+        # Exiting
         else:
             print("Terminating operations...")
             break
 
-        choice = main_menu()
+        choice = interactions.main_menu()
 
 if __name__ == "__main__":
     main()
