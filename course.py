a5ec3374797faa6e1b0a0ed9ee73f82492ceb1e2
Mikael-UofA <lnineza@ualberta.ca>
2024-07-28 14:30:45 -0600

    create course.py

---
+++ course.py
@@ -0,0 +1,15 @@
+class Course:
+    def __init__(self, subject_code, course_code, course_title, year, semester, letter_grade, instructor, comment):
+        self.subject_code = subject_code
+        self.course_code = course_code
+        self.course_title = course_title
+        self.year = year
+        self.semester = semester
+        self.letter_grade = letter_grade
+        self.instructor = instructor
+        self.comment = comment
+
+    def print(self):
+        print(f"{self.subject_code} {self.course_code}: {self.course_title}\n"
+              f"{self.semester} Semester\n"
+              f"Taught by {self.instructor}, Grade: {self.letter_grade}")
