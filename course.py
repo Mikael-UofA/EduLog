400078f17f2e239e8607d6e228b408f5c213f0b2
Mikael-UofA <lnineza@ualberta.ca>
2024-07-28 18:59:04 -0600

    changed title attribute name

--- course.py
+++ course.py
@@ -1,8 +1,8 @@
 class Course:
-    def __init__(self, subject_code, course_code, course_title, year, semester, letter_grade, instructor, comment):
+    def __init__(self, subject_code, course_code, title, year, semester, letter_grade, instructor, comment):
         self.subject_code = subject_code
         self.course_code = course_code
-        self.course_title = course_title
+        self.title = title
         self.year = year
         self.semester = semester
         self.letter_grade = letter_grade
@@ -10,6 +10,6 @@ class Course:
         self.comment = comment
 
     def print(self):
-        print(f"{self.subject_code} {self.course_code}: {self.course_title}\n"
-              f"{self.semester} Semester\n"
-              f"Taught by {self.instructor}, Grade: {self.letter_grade}\n Notes: {self.comment}")
+        print(f"{self.subject_code} {self.course_code}: {self.title}\n"
+              f"{self.semester} {self.year}\n"
+              f"Taught by {self.instructor}, Grade: {self.letter_grade}\nNotes: {self.comment}")
